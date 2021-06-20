bd574029f4f67a313fd5d38877b7d4638dc7c384
Hiroto Yamakawa <1794095+gishi-yama@users.noreply.github.com>
2021-06-20 16:47:27 +0900

    コメントアウトを解除

--- src/main/java/com/example/fitbit/WebSocketConfig.java
+++ src/main/java/com/example/fitbit/WebSocketConfig.java
@@ -1,24 +1,26 @@
 package com.example.fitbit;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
 
-//  private final FitBitWebSocketHandler fitBitWebSocketHandler;
-//
-//  @Autowired
-//  public WebSocketConfig(FitBitWebSocketHandler fitBitWebSocketHandler) {
-//    this.fitBitWebSocketHandler = fitBitWebSocketHandler;
-//  }
-//
-//  @Override
-//  public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
-//    webSocketHandlerRegistry.addHandler(fitBitWebSocketHandler, "/edge")
-//      .setAllowedOrigins("*");
-//  }
+  private final FitBitWebSocketHandler fitBitWebSocketHandler;
+
+  @Autowired
+  public WebSocketConfig(FitBitWebSocketHandler fitBitWebSocketHandler) {
+    this.fitBitWebSocketHandler = fitBitWebSocketHandler;
+  }
+
+  @Override
+  public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
+    webSocketHandlerRegistry.addHandler(fitBitWebSocketHandler, "/edge")
+      .setAllowedOrigins("*");
+  }
 
 }
