22ad307604b3b130fabca5591fa7239b862d79f3
Hiroto Yamakawa <1794095+gishi-yama@users.noreply.github.com>
2024-05-30 20:21:58 +0900

    Bump up Spring Boot

--- src/main/java/com/example/fitbit/MyHeartRateAPI.java
+++ src/main/java/com/example/fitbit/MyHeartRateAPI.java
@@ -1,12 +1,12 @@
 package com.example.fitbit;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
